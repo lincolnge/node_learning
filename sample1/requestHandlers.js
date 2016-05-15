ad61751bdeb7725a814825a72eee1e1869ccad8c
王耕洲 <wanggengzhou@meituan.com>
2016-05-15 19:45:37 +0800

    Change /start response to be a form

--- sample1/requestHandlers.js
+++ sample1/requestHandlers.js
@@ -4,13 +4,22 @@ function start(response) {
     console.log('Request handler "start" was called.');
     var content = 'empty';
 
-    exec('find /',
-        { timeout: 10000, maxBuffer: 20000*1024 },
-        function (error, stdout, stderr) {
-            response.writeHead(200, {'Content-Type': 'text/plain'});
-            response.write(stdout);
-            response.end();
-        });
+    var body = '<html>' +
+        '<head>' +
+        '<meta http-equiv="Content-Type" content="text/html; ' +
+        'charset=UTF-8" />' +
+        '</head>' +
+        '<body>' +
+        '<form action="/upload" method="post">' +
+        '<textare name="text" rows="20" cols="60"></textare>' +
+        '<input type="submit" value="Submit text" />' +
+        '</form>' +
+        '</body>' +
+        '</html>';
+
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(body);
+        response.end();
 }
 
 function upload(response) {
