ce322ae06ed06654207a2534f9312738aa7273f9
王耕洲 <wanggengzhou@meituan.com>
2016-05-15 20:08:59 +0800

    Create form and send it.

--- sample1/requestHandlers.js
+++ sample1/requestHandlers.js
@@ -1,6 +1,6 @@
-var exec = require('child_process').exec;
+var queryString = require('querystring');
 
-function start(response) {
+function start(response, postData) {
     console.log('Request handler "start" was called.');
     var content = 'empty';
 
@@ -22,10 +22,10 @@ function start(response) {
         response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
     console.log('Request handler "upload" was called.');
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello Upload');
+    response.write('You\'ve send the text: ' + queryString.parse(postData).text);
     response.end();
 }
 
